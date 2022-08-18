bf371c21585cdd61a0085af3cf7ab37c5828d6ea
mektigboy <mektigboy@protonmail.com>
2022-08-18 14:14:22 -0500

    Update helper-hardhat-config.js

--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -1,35 +1,24 @@
 const developmentChains = ["hardhat", "localhost"];
 
+const DECIMALS = "18";
+const INITIAL_PRICE = "200000000000000000000"; // 2e20
+
 const networkConfig = {
-    default: {
-        keepersUpdateInterval: "30",
-        name: "hardhat",
-    },
-    1: {
-        keepersUpdateInterval: "30",
-        name: "mainnet",
-    },
     4: {
-        callbackGasLimit: "500000", // 500,000 gas
-        keepersUpdateInterval: "30",
-        gasLane:
-            "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc", // 30 gwei
+        ethUsdPriceFeed: "0x8A753747A1Fa494EC906cE90E9f37563A8AF630e",
+        mintFee: "10000000000000000", // 0.01 ETH
         name: "rinkeby",
-        raffleEntranceFee: "100000000000000000", // 0.1 ETH
-        subscriptionId: "9747", // Subscription id.
     },
     31337: {
-        callbackGasLimit: "500000", // 500,000 gas
-        keepersUpdateInterval: "30",
-        gasLane:
-            "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc", // 30 gwei
+        ethUsdPriceFeed: "0x9326BFA02ADD2366b30bacB125260Af641031331",
+        mintFee: "10000000000000000", // 0.01 ETH
         name: "localhost",
-        raffleEntranceFee: "100000000000000000", // 0.1 ETH
-        subscriptionId: "9747",
     },
 };
 
 module.exports = {
     developmentChains,
+    DECIMALS,
+    INITIAL_PRICE,
     networkConfig,
 };
